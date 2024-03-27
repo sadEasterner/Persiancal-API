98460aa8432ddc98bebc473d6511ee7f50362618
sadEasterner <hoseinseifig1380@gmail.com>
2024-03-27 19:11:20 +0330

    [Fix] : Fixed problem in ordering middlewares

--- routes/products.ts
+++ routes/products.ts
@@ -3,13 +3,14 @@ import Router from 'express';
 import productController from '../controllers/productController';
 productController
 import { verifyRoles, ROLES_LIST } from '../middleware/verifyRoles';
+import { verifyJWT } from '../middleware/verifyJWT';
 const router = Router();
 
 router.route('/:id').get(productController.getProductById);
-router.route('').post(verifyRoles(ROLES_LIST.Admin), productController.createProduct);
+router.route('').post(verifyJWT, verifyRoles(ROLES_LIST.Admin), productController.createProduct);
 router.route('/getList').post(productController.getProducts);
-router.route('').put(verifyRoles(ROLES_LIST.Admin), productController.editProduct);
-router.route('/changeStatus').put(verifyRoles(ROLES_LIST.Admin), productController.changeProductStatus);
-router.route('').delete(verifyRoles(ROLES_LIST.Admin), productController.changeProductStatus);
+router.route('').put(verifyJWT, verifyRoles(ROLES_LIST.Admin), productController.editProduct);
+router.route('/changeStatus').put(verifyJWT, verifyRoles(ROLES_LIST.Admin), productController.changeProductStatus);
+router.route('').delete(verifyJWT, verifyRoles(ROLES_LIST.Admin), productController.changeProductStatus);
 
 export default router;
--- routes/user.ts
+++ routes/user.ts
@@ -2,17 +2,16 @@
 import Router from 'express';
 import userController from '../controllers/userController';
 import { verifyRoles, ROLES_LIST } from '../middleware/verifyRoles';
-import { verifyJWT } from '../middleware/verifyJWT';
 const router = Router();
 
 router.route('/:username').get(userController.getUserByUsername);
 
-router.route('').post(verifyJWT, verifyRoles(ROLES_LIST.Admin), userController.createUser);
+router.route('').post(verifyRoles(ROLES_LIST.Admin), userController.createUser);
 router.route('/getList').post(userController.getUsers);
 
-router.route('').put(verifyJWT, verifyRoles(ROLES_LIST.Admin), userController.editUser);
-router.route('/changeStatus').put(verifyJWT, verifyRoles(ROLES_LIST.Admin), userController.changeUserStatus);
+router.route('').put(verifyRoles(ROLES_LIST.Admin), userController.editUser);
+router.route('/changeStatus').put( verifyRoles(ROLES_LIST.Admin), userController.changeUserStatus);
 
-router.route('').delete(verifyJWT, verifyRoles(ROLES_LIST.Admin), userController.changeUserStatus);
+router.route('').delete(verifyRoles(ROLES_LIST.Admin), userController.changeUserStatus);
 
 export default router;

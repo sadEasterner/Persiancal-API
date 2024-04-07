81419a1b69a35d108486dfffa054b42fef8ca7cb
amirhosein <ah.t222amir22@gmail.com>
2024-04-07 12:28:42 +0330

    define lab model

---
+++ models/labs.ts
@@ -0,0 +1,26 @@
+import { DataTypes } from 'sequelize';
+import sequelize from '../utils/database';
+import { LAB_STATUS } from '../config/parameters/lab-status';
+
+const Labs = sequelize.define('lab',{
+    id: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        primaryKey: true
+    },
+    name:{
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    description: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    labStatus: {
+        type: DataTypes.INTEGER,
+        defaultValue: LAB_STATUS.Active,
+        allowNull: false
+    },
+});
+
+module.exports = Labs;

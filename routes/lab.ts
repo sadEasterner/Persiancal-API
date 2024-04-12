d6a9963bf87165c8a9196c84d490ad7c7eb819db
amirhosein <ah.t222amir22@gmail.com>
2024-04-13 00:07:30 +0330

    [FIX]: set verifyJWT middleware for lab routes

--- routes/lab.ts
+++ routes/lab.ts
@@ -1,16 +1,22 @@
-import Router from 'express';
-import { verifyRoles, ROLES_LIST } from '../middleware/verifyRoles';
-import labController from '../controllers/labController';
+import Router from "express";
+import { verifyRoles, ROLES_LIST } from "../middleware/verifyRoles";
+import labController from "../controllers/labController";
+import { verifyJWT } from "../middleware/verifyJWT";
 const router = Router();
 
-router.route('/getList').get(labController.getLabs);
-router.route('/getById/:id').get(labController.getLabById);
+router.route("/getList").get(labController.getLabs);
+router.route("/getById/:id").get(labController.getLabById);
 
-router.route('').post(verifyRoles(ROLES_LIST.Admin), labController.createLab);
+router
+  .route("")
+  .post(verifyJWT, verifyRoles(ROLES_LIST.Admin), labController.createLab);
 
-router.route('').put(verifyRoles(ROLES_LIST.Admin), labController.editLab);
-// router.route('/changeStatus').put(verifyRoles(ROLES_LIST.Admin), labController.changeLabStatus);
+router
+  .route("")
+  .put(verifyJWT, verifyRoles(ROLES_LIST.Admin), labController.editLab);
 
-router.route('/:id').delete(verifyRoles(ROLES_LIST.Admin), labController.deleteLab);
+router
+  .route("/:id")
+  .delete(verifyJWT, verifyRoles(ROLES_LIST.Admin), labController.deleteLab);
 
 export default router;

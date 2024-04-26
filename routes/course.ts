46eaaabfe0e18baf0486ea73960ff54086326769
amirhosein <ah.t222amir22@gmail.com>
2024-04-26 19:09:33 +0330

    fix courses bug

--- routes/course.ts
+++ routes/course.ts
@@ -1,15 +1,30 @@
-import Router from 'express';
-import { verifyRoles, ROLES_LIST } from '../middleware/verifyRoles';
-import courseController from '../controllers/courseController';
+import Router from "express";
+import { verifyRoles, ROLES_LIST } from "../middleware/verifyRoles";
+import courseController from "../controllers/courseController";
+import { verifyJWT } from "../middleware/verifyJWT";
 const router = Router();
 
-router.route('/getList').get(courseController.getCourses);
-router.route('/getById/:id').get(courseController.getCourseById);
+router.route("/getList").get(courseController.getCourses);
+router.route("/getById/:id").get(courseController.getCourseById);
 
-router.route('').post(verifyRoles(ROLES_LIST.Admin), courseController.createCourse);
+router
+  .route("")
+  .post(
+    verifyJWT,
+    verifyRoles(ROLES_LIST.Admin),
+    courseController.createCourse
+  );
 
-router.route('').put(verifyRoles(ROLES_LIST.Admin), courseController.editCourse);
+router
+  .route("")
+  .put(verifyJWT, verifyRoles(ROLES_LIST.Admin), courseController.editCourse);
 
-router.route('/:id').delete(verifyRoles(ROLES_LIST.Admin), courseController.deleteCourse);
+router
+  .route("/:id")
+  .delete(
+    verifyJWT,
+    verifyRoles(ROLES_LIST.Admin),
+    courseController.deleteCourse
+  );
 
 export default router;

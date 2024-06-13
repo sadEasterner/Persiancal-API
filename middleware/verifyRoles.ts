3295e217b0a3020329efd862a37d18ea35522cc5
sadEasterner <hoseinseifig1380@gmail.com>
2024-06-13 17:24:24 +0330

    [Fix]: Fixed bug in create product method

--- middleware/verifyRoles.ts
+++ middleware/verifyRoles.ts
@@ -1,24 +1,31 @@
-import {ROLES_LIST} from '../config/parameters/roles-list'
-import { NextFunction, Request, Response } from 'express';
-import { LOG_TYPE, logger } from './logEvents';
-import { AuthenticatedRequest } from '../interfaces/requests/IAuthenticatedRequest';
-
-
-
+import { ROLES_LIST } from "../config/parameters/roles-list";
+import { NextFunction, Request, Response } from "express";
+import { LOG_TYPE, logger } from "./logEvents";
+import { AuthenticatedRequest } from "../interfaces/requests/IAuthenticatedRequest";
 
 const verifyRoles = (...alloedRoles: any) => {
-    return (req:AuthenticatedRequest, res:Response, next:NextFunction) => {
-        const rolesArray = [...alloedRoles];
-        if(!req.currentRole ) {
-            logger(LOG_TYPE.Error,"user have no roles", "middleware",'middleware/verifyRoles/line-15');
-            return res.status(401);
-        }
-        const result = rolesArray.includes(req.currentRole);
-        if(!result) {
-            logger(LOG_TYPE.Error,"user have no access", "middleware",'middleware/verifyRoles/line-20');
-            return res.status(403).json({message: "user have no access"});
-        }
-        next();
-    };
+  return (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
+    const rolesArray = [...alloedRoles];
+    if (!req.currentRole) {
+      logger(
+        LOG_TYPE.Error,
+        "user have no roles",
+        "middleware",
+        "middleware/verifyRoles/line-15"
+      );
+      return res.status(401);
+    }
+    const result = rolesArray.includes(req.currentRole);
+    if (!result) {
+      logger(
+        LOG_TYPE.Error,
+        "user have no access",
+        "middleware",
+        "middleware/verifyRoles/line-20"
+      );
+      return res.status(403).json({ message: "user have no access" });
+    }
+    next();
+  };
 };
-export {verifyRoles, ROLES_LIST};
+export { verifyRoles, ROLES_LIST };

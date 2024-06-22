784633add5638dc9242f351dd8603b6f6a275cf5
amirhosein <ah.t222amir22@gmail.com>
2024-06-22 20:50:13 +0330

    add duration to course model as integer

--- models/courses.ts
+++ models/courses.ts
@@ -1,30 +1,34 @@
-import { DataTypes } from 'sequelize';
-import sequelize from '../utils/database';
-import { COURSE_STATUS } from '../config/parameters/course-status';
+import { DataTypes } from "sequelize";
+import sequelize from "../utils/database";
+import { COURSE_STATUS } from "../config/parameters/course-status";
 
-const Courses = sequelize.define('course',{
-    id: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        primaryKey: true
-    },
-    title:{
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    description: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    provider: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    courseStatus: {
-        type: DataTypes.INTEGER,
-        defaultValue: COURSE_STATUS.Active,
-        allowNull: false
-    },
+const Courses = sequelize.define("course", {
+  id: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    primaryKey: true,
+  },
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  provider: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  courseStatus: {
+    type: DataTypes.INTEGER,
+    defaultValue: COURSE_STATUS.Active,
+    allowNull: false,
+  },
+  duration: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
 });
 
 module.exports = Courses;

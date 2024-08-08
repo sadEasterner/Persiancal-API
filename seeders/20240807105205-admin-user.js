08e621258fc286e24a58e3b06fd7a6913a81f697
amirhosein <ah.t222amir22@gmail.com>
2024-08-08 18:06:14 +0330

    init seeder

---
+++ seeders/20240807105205-admin-user.js
@@ -0,0 +1,27 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    return queryInterface.bulkInsert("users", [
+      {
+        username: "admin",
+        name: "admin",
+        email: "example@example.com",
+        password: "adminpass123",
+        role: 2,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  },
+};

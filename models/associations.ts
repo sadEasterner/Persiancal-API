fcbe2129d1fbbd6cdb0091d914203db95e316c85
amirhosein <ah.t222amir22@gmail.com>
2024-08-10 21:14:53 +0330

    change associations

--- models/associations.ts
+++ models/associations.ts
@@ -1,15 +1,32 @@
-const Products = require("./products");
-const Labs = require("./labs");
-const ImageUrls = require("./imageUrls");
+// const Products = require("./products");
+// const Labs = require("./labs");
+// const ImageUrls = require("./imageUrls");
 
-// Labs
-Labs.hasMany(ImageUrls, { foreignKey: "itemId", as: "labImages" });
-ImageUrls.belongsTo(Labs, { foreignKey: "itemId", as: "lab" });
-// products association
-Products.hasMany(ImageUrls, { foreignKey: "itemId", as: "productImages" });
-ImageUrls.belongsTo(Products, {
-  foreignKey: "itemId",
+// // Labs
+// Labs.hasMany(ImageUrls, { foreignKey: "itemId", as: "labImages" });
+// ImageUrls.belongsTo(Labs, { foreignKey: "itemId", as: "lab" });
+// // products association
+// Products.hasMany(ImageUrls, { foreignKey: "itemId", as: "productImages" });
+// ImageUrls.belongsTo(Products, {
+//   foreignKey: "itemId",
+//   as: "product",
+// });
+
+// export { ImageUrls, Labs, Products };
+
+const ProductImageUrls = require("../models/productImageUrls");
+const Products = require("../models/products");
+
+const LabImageUrls = require("../models/labImageUrls");
+const Labs = require("../models/labs");
+
+Products.hasMany(ProductImageUrls, { foreignKey: "productId", as: "productImages" });
+ProductImageUrls.belongsTo(Products, {
+  foreignKey: "productId",
   as: "product",
 });
 
-export { ImageUrls, Labs, Products };
+Labs.hasMany(LabImageUrls, { foreignKey: "labId", as: "labImages" });
+LabImageUrls.belongsTo(Labs, { foreignKey: "labId", as: "lab" });
+
+export { Products, ProductImageUrls, Labs, LabImageUrls };

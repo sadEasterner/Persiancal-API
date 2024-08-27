d1e4edb6d2d6d3599b99deb4f591df714b3bc487
amirhosein <ah.t222amir22@gmail.com>
2024-08-27 20:13:16 +0330

    debug path

--- config/config.js
+++ config/config.js
@@ -1,9 +1,9 @@
 module.exports = {
   development: {
-    username: "root",
-    password: "",
-    database: "",
-    host: "localhost",
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    host: process.env.DATABASE_HOST,
     dialect: "mysql",
   },
   test: {

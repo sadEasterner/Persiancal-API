9d6320220258a440d02a3bb77bde66f3f73ef5ef
amirhosein <ah.t222amir22@gmail.com>
2024-08-27 20:05:40 +0330

    configs read from .env

--- utils/database.ts
+++ utils/database.ts
@@ -1,8 +1,13 @@
-require('dotenv').config();
-import { Sequelize } from 'sequelize';
-const sequelize = new Sequelize(process.env.DATABASE_NAME!, process.env.DATABASE_USERNAME!, process.env.DATABASE_PASSWORD!, {
-    dialect: 'mysql',
-    host: 'localhost'
-})
+require("dotenv").config();
+import { Sequelize } from "sequelize";
+const sequelize = new Sequelize(
+  process.env.DATABASE_NAME!,
+  process.env.DATABASE_USERNAME!,
+  process.env.DATABASE_PASSWORD!,
+  {
+    dialect: "mysql",
+    host: process.env.DATABASE_HOST,
+  }
+);
 
 export default sequelize;
